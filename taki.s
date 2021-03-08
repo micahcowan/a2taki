5c041a58c624aaf898db72ae8ec183febf855674
Micah Cowan <micah@addictivecode.org>
2021-03-08 04:06:10 -0800

    Flash, Inverse effects (likely DOESN'T WORK)

--- taki.s
+++ taki.s
@@ -1,17 +1,41 @@
-;;   Eventually: detect and save aside DOS/ProDOS processors
-;;   so that we can detect Ctrl-D at beginning of line,
-;;   and switch things over to DOS/ProDOS at that point
+;; taki.s
 
-    .include "progstart.inc"
+.include "progstart.inc"
+.macpack apple2
 
-MON_COut = $FDF0
-DOS_CSWL = $AA53
-CSWL     = $36
+MON_COut    = $FDF0
+MON_InvFlag = $32
+DOS_CSWL    = $AA53
+CSWL        = $36
 
-.define TakiControlChar #$9A   ; Control-T (for "Taki")
+InstanceDataBuffer = TAKISTART - $100
 
-.define ST_Init             #$00
-.define ST_EffectName       #$01
+zpCurrentEffectL = $06
+zpCurrentEffectH = $07
+
+.enum   State
+        Default
+        EffectSelect
+        EffectText
+.endenum
+
+; Indices to the HIGH byte of an effect's jump table.
+.enum   EffectJump
+        Init        = 1
+        Text        = 3
+        TextEnd     = 5
+        Tick        = 7
+.endenum
+
+.define CTRL(char)  (char - $40 + $80)
+
+; TakiCommandChar (Ctrl-T): Use this to send commands/configure Taki.
+.define TakiCommandChar #CTRL('T')
+; TakiSelectChar (Ctrl-R): Follow this with a pre-arranged character
+; to select the text that follows, to receive a text effect. Text
+; is captured for the effect until the next occurrence
+; of TakiSelectChar.
+.define TakiSelectChar #CTRL('R')
 
 ;; Public interfaces ;;;;;;;;;;;;;;;;;;;
 
@@ -25,6 +49,60 @@ CallInit:
     JMP Init
 CallTakiPrint:
     JMP TakiPrint
+
+;; Vars
+RunOurCSW:
+    .byt $4C    ; JMP
+vOurCSWL:
+    .byt $00
+    .byt $00
+
+vState:
+    .byt $00
+
+vCurrentEDX:
+    .byt $00
+
+;; Tables
+
+; EffectsTable
+; 
+; Lists all the Effects' jump table addresses, so that they can
+; be mapped to a single-byte EDX (Effect Index), used to refer
+; to that effect everywhere else.
+_EffectsTable:
+    EF_Flash_EDX = 2
+    .word EF_Flash
+
+    EF_Inverse_EDX = 4
+    .word EF_Inverse
+
+    .word $0000
+
+; No effect is allowed to have an EDX of 0 (we need as a sentinel value),
+; so we pretend the EffectsTablee actually starts one entry
+; (word, = 2 bytes) earlier.
+EffectsTable = _EffectsTable - 2
+
+; TODO: This table will be initialized at start and configured by the user
+;       but for right now it's hard-coded. Must always have a valid
+;       fall-back profile (FLASH) as its first entry.
+;
+;       For right now, this table looks up directly to an EDX;
+;       later it will look up into a configuration string to use for
+;       instantiating an effect with a particular configuration.
+EffectProfilesTable:
+    .byt 'F', EF_Flash_EDX
+    .byt 'I', EF_Inverse_EDX
+    .byt $00, $00
+
+; EffectInstancesTable
+;
+; List of indexes into InstanceDataBuffer, $FF-terminated
+EffectInstancesTable:
+    .res 32, $FF
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 Init:
     ; Install for DOS 3.3 CSW
@@ -43,24 +121,21 @@ Init:
     STA vOurCSWL
     LDA #>MON_COut
     STA vOurCSWL+1
-
+    ; - fall-through -
+PageInit:
     ; Initialize processing state
-    LDA ST_Init
+    LDA #State::Default
     STA vState
+    ; TODO: Initialize effect profiles
+    ;LDA #$00
+    ;STA EffectProfilesTable
+
+    ; Initialize effect instances
+    ; TODO: run a cleanup/destructor on old instances first?
+    LDA #$00
+    STA EffectInstancesTable
 
     RTS
-
-;; Vars
-RunOurCSW:
-    .byt $4C    ; JMP
-vOurCSWL:
-    .byt $00
-    .byt $00
-
-vState:
-    .byt $00
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 TakiPrint:
     CLD         ; ProDOS wants this in any user routine it calls
@@ -78,17 +153,23 @@ TakiPrint:
     LDA $100, X
     ; What state are we in?
     LDX vState
-    ;BNE StateChk    ; TODO
+    BNE StateChk    ; TODO
 
     ; Zero. Default, pass-it-through state.
-    ; ...Is the character our special control character?
-    CMP TakiControlChar
+    ; ...Is the character a special control character?
+    CMP TakiSelectChar
     BNE CookedOutput
     ; It IS our special control. Enter effect name-parsing
-    LDX ST_EffectName
+    LDX #State::EffectSelect
     STX vState
-    ; TODO initialize effect name buffer here
-    ;JMP ParseEffectName
+    BNE TakiPrintExit   ; ALWAYS
+StateChk:
+    CPX #State::EffectSelect
+    BEQ EffectSelect
+    CPX #State::EffectText
+    BEQ EffectText
+    ; ERROR! We are in a state that doesn't exist!
+    BRK
 CookedOutput:
     JSR RunOurCSW
 TakiPrintExit:
@@ -98,3 +179,101 @@ TakiPrintExit:
     TAX
     PLA
     RTS
+
+EffectSelect:
+    ; Look up the effect profile for the current character,
+    ; And an instantiate that effect
+@FindEffect:
+    LDX #$00
+      LDY EffectProfilesTable, X
+      BEQ @NullEffectSelected   ; We didn't find the effect!
+      CMP EffectProfilesTable, X
+      BEQ @Instantiate
+      ; Advance to next entry
+      INX
+      INX
+    JMP @FindEffect
+@NullEffectSelected:
+    LDX #$00
+@Instantiate:
+    ; TODO: Instantiate the effect
+    ; Set the current effect from EDX
+    INX ; advance to the EDX
+    LDY EffectProfilesTable, X
+    STY vCurrentEDX
+    LDA EffectsTable, Y
+    STA zpCurrentEffectL
+    LDA EffectsTable+1, Y
+    STA zpCurrentEffectH
+    ; ...and set the state to "feeding text to the effect" mode
+    LDY #State::EffectText
+    STY vState
+    JMP TakiPrintExit
+
+EffectText:
+    ; Did we reach the end of the effect's text?
+    CMP TakiSelectChar
+    BEQ @EndText
+    ; ...no. Still feeding text to the effect
+    STY EffectJump::Text
+    JSR RunCurrentEffect
+    JMP TakiPrintExit
+@EndText:
+    ; exit text-feeeding
+    LDA #State::Default
+    STA vState
+    STY EffectJump::TextEnd
+    JSR RunCurrentEffect
+    JMP TakiPrintExit
+
+RunCurrentEffect:
+    TAX
+    LDA (zpCurrentEffectL), Y
+    PHA
+    DEY
+    LDA (zpCurrentEffectL), Y
+    PHA
+    TXA
+    RTS     ; actually a JUMP, since we just pushed an address.
+
+; Include effects definitions:
+;.include "e_flash.inc"
+;.include "e_inverse.inc"
+
+EF_Flash:
+    .word   EF_Flash_Init - 1
+    .word   EF_Flash_Text - 1
+    .word   EF_Flash_TextEnd - 1
+    .word   EF_Flash_Tick - 1
+    .word   $0000
+
+EF_Flash_Init:
+EF_Flash_TextEnd:
+EF_Flash_Tick:
+    RTS
+
+EF_Flash_Text:
+    ORA #$40    ; The "flash" bit
+    JMP (vOurCSWL)
+    ; RTS not needed
+
+EF_Inverse:
+    .word   EF_Flash_Init - 1
+    .word   EF_Inverse_Text - 1
+    .word   EF_Inverse_TextEnd - 1
+    .word   EF_Flash_Tick - 1
+    .word   $0000
+
+EF_Inverse_Text:
+    ; Clear the "mask-out-inverse" flag from the monitor
+    LDX #$3F
+    STX MON_InvFlag
+    JMP (vOurCSWL)
+    ; RTS not needed
+
+EF_Inverse_TextEnd:
+    ; Set the "mask-out-inverse" flag from the monitor
+    LDX #$7F
+    STX MON_InvFlag
+    JMP (vOurCSWL)
+    ;[L/H] RTS not needed
