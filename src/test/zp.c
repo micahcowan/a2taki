be3fae7bfe04663f8c127fc9df6437fe0268c764
Micah Cowan <micah@addictivecode.org>
2021-03-10 17:56:10 -0800

    Keep "MAIN" section out of the way of testlib firmware.

--- src/test/zp.c
+++ src/test/zp.c
@@ -8,12 +8,22 @@
 #define ZP  ((unsigned char *)(0x0000))
 #define MIN_USE  0x80
 
+/* Handy routine for filling the stack up with values */
+#if 0
+void callABunch(int depth, int var1, int var2, int var3, int var4, int var5, int var6, int var7, int var8, int var9, int var10, int var11, int var12, int var13, int var14, int var15, int var16) {
+    if (depth != 0)
+        callABunch(depth-1, var1, var2, var3, var4, var5, var6, var7, var8, var9, var10, var11, var12, var13, var14, var15, var16);
+    return;
+}
+#endif
+
 int main(void)
 {
     unsigned char *p = ZP;
     unsigned char *zpe = ZP + 0x100;
     unsigned char *q = zpe;
 
+    //callABunch(30, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16);
     puts("Zero page contents:\n");
     while (p - ZP != 255) {
         unsigned char col = (p - ZP) % 8;
