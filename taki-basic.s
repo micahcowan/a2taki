1fb3275f7406f0d96ff3f8f6c12f79f5f5b76c62
Micah Cowan <micah@addictivecode.org>
2022-01-31 21:24:03 -0800

    Abandon ampersand (&) processing for control chars
    Can't use ampersand commands from assembly code, after all...

--- taki-basic.s
+++ taki-basic.s
@@ -3,10 +3,13 @@
 .export ASoftProg, ASoftEnd
 
 ASoftProg:
-scrcode "100 PRINT ",'"',"HELLO",'"',$0D
-scrcode "105 PRINT ",'"',"THIS IS AN EXAMPLE OF A ",'"',';',$0D
-scrcode "110 & ",'"',"TAKI LINE SCAN",'"',$0D
-scrcode "120 PRINT ",'"',"SCANNING",'"',$0D
+scrcode "CALL 32768:REM  START TAKI",$0D
+scrcode "10 R$=CHR$(ASC(",'"',"R",'"',")-64)",$0D
+;scrcode "50 HOME",$0D
+scrcode "95 PRINT ",'"',"THIS ISN'T INCLUDED",'"',$0D
+scrcode "105 PRINT ",'"',"HELLO, THIS IS AN EXAMPLE",'"',$0D
+scrcode "110 PRINT ",'"',"OF A ",'"',';',$0D
+scrcode "120 PRINT R$;",'"',"SSCANNING",'"',";R$;",$0D
 scrcode "130 PRINT ",'"'," WORD",'"',$0D
 scrcode "RUN",$0D
 ASoftEnd:
--- taki.s
+++ taki.s
@@ -1,3 +1,4 @@
+;#link "taki-cout.s"
 ;#link "taki-basic.s"
 ;#link "load-and-run-basic.s"
 ;#resource "taki.cfg"
@@ -7,36 +8,49 @@
 
 .include "taki-util.inc"
 
+CSWL	= $36
+TEXTTAB	= $67
+VARTAB	= $69
+PRGEND	= $AF
 CHRGET	= $B1
 CHRGOT	= $B7
-AMPERV	= $3F5
 
 .segment "STARTUP"
 TakiStartup:
-	jsr PTakiInit
 	jmp LoadAndRunBasic
         
 .segment "CODE"
 
 ; Stable entry points table
+PTakiMoveASoft:
+	jmp TakiMoveASoft
 PTakiInit:
 	jmp TakiInit
 
-TakiKeyword:
-	tstr "TAKI"
-TakiCommands:
-	tstr "LINE"
-        .byte $00
+; Reorganize where in memory a BASIC program is
+; located (to move it out of the way of the
+; second text page - to $C01)
+TakiMoveASoft:
+	lda #$0C
+        sta TEXTTAB+1
+        sta VARTAB+1
+        sta PRGEND+1
+	rts
+
+; Initialize Taki, hijacking input and output
+; for special processing (and to send things to both
+; text pages)
 TakiInit:
 	; Initialize & to skip to end of line and RTS
-        writeByte AMPERV, $4C;	"JMP"
-        writeWord {AMPERV+1}, AmperHandle
         rts
 
-AmperHandle:
-	; Just skip until NUL byte
-        cmp CHRGOT
-        beq @done
-:       jsr CHRGET
-        bne :-
-@done:	rts
+.if 0
+; not done writing this fn
+PrintStr:
+PrintStrAddr = PrintStr+1
+	lda #1000	; Overwritten before call.
+        		; Load next str char. Is NUL?
+        beq PSDone	; yes: exit
+PSDone: rts
+.endif
+        
