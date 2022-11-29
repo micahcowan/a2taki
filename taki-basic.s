8350d10bab0824c0d94d4aca3c67ac8fdef27487
Micah Cowan <micah@addictivecode.org>
2022-11-28 22:18:42 -0800

    Minor quoting tweaks in AS listing

--- taki-basic.s
+++ taki-basic.s
@@ -26,7 +26,7 @@ scrcode   ":REM TAKI DEBUG MODE",$0D
 ;line "PRINT ",'"',"1234567890123456789012345678901234567890",'"',';',$0D
 line "PRINT ",'"',"HELLO, THIS IS AN EXAMPLE",'"',$0D
 line "PRINT ",'"',"OF A ",'"',';',$0D
-line "PRINT T$;",'"',"MARK SCAN(FDLY=10)",$0D
+line "PRINT T$;",'"',"MARK SCAN(FDLY=10)",'"',$0D
 line "PRINT ",'"',"< SCANNING >",'"',";Q$;",$0D
 line "PRINT ",'"'," WORD",'"',":?",$0D
 line "PRINT ",'"',"NOTHING HAPPENS",'"',';',$0D
@@ -35,7 +35,7 @@ line "PRINT ",'"',"HERE",'"',":PRINT",$0D
 line "PRINT T$",'"',"OR HERE",'"',":REM BAD CMD",$0D
 line "PRINT ",'"',"OH, AND ALSO YOU MAY BE INTERESTED",'"',$0D
 line "PRINT ",'"',"TO FIND THAT HERE IS ",'"',';',$0D
-line "PRINT T$;",'"',"WORD SCAN(FDLY=96)",$0D
+line "PRINT T$;",'"',"WORD SCAN(FDLY=96)",'"',$0D
 line "PRINT ",'"',"ANOTHER SUCH WORD",'"',":?:?",$0D
 
 line "PRINT T$;",'"',"INSTANT SPINR",'"',$0D
